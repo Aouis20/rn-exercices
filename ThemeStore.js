751f0cef2a5e198f39c28be6fddc672d56406024
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2022-09-23 17:44:29 +0200

    Delete ThemeStore.js

--- ThemeStore.js
+++
@@ -1,7 +0,0 @@
-import createStore from 'zustand';
-import { initialStateThemeStore, ThemeReducer } from './ThemeReducer';
-
-export const useThemeStore = createStore((set) => ({
-  ...initialStateThemeStore,
-  dispatch: (args) => set((state) => ThemeReducer(state, args)),
-}));
